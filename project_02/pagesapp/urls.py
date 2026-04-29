41da16b7a77ad36595b2743eea24d9ab12bdb89d
xasan <muhammadovich.09@gmail.com>
2026-04-29 14:03:29 +0500

    remove all files

--- project_02/pagesapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomePageView, AboutPageView, NewsPageView
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name = 'about'),
-    path('news/', NewsPageView.as_view(), name = 'news' ),
-]
